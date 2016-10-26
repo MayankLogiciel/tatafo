549f9b7f5003c65ff4dd5a8ed7a01456e6b48d94
sachinlogiciel <sachin.jain@logicielsolutions.co.in>
2016-10-26 12:37:52 +0530

    update header search and ui fixes

--- www/js/directives/components.directive.js
+++ www/js/directives/components.directive.js
@@ -8,6 +8,23 @@
 		    	templateUrl: 'views/app/feeds/feed-listing-item.html',
 		    	replace: true
 		  	};
+		})
+
+
+		.directive('actionEnter', function () {
+		    return function (scope, element, attrs) {
+		        element.bind("keydown keypress", function (event) {
+		            if (event.which === 13) {
+		                scope.$apply(function () {
+		                    scope.$eval(attrs.actionEnter, {
+		                        'event': event
+		                    });
+		                });
+
+		                event.preventDefault();
+		            }
+		        });
+		    };
 		});
 })();
 
