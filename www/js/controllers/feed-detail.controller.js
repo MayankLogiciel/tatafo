284fa8cd20cb84a3b31078cc8a9f22e403cf41ff
Manpreet <manpreet.singh@logicielsolutions.co.in>
2016-11-19 16:13:34 +0530

    Offline Message and Feed detail controller uncomment

--- www/js/controllers/feed-detail.controller.js
+++ www/js/controllers/feed-detail.controller.js
@@ -15,8 +15,7 @@
 			$scope.allFeed=[];
 			$scope.load = false;
 			$scope.entry = feedDetailService.getPostData(); //load feed data
-			console.log($scope.entry);
-			//applyImageLoaderToDetailFeedImages($scope.entry);
+			applyImageLoaderToDetailFeedImages($scope.entry);
 			$scope.load = true;
 			//find related source information from localStorage saved sources
 			$scope.sourceData = sourcesService.getFeedSourceFromLocalStorage($scope.entry.source_id);
@@ -40,24 +39,24 @@
 		* applyImageLoaderToDetailFeedImages is used to apply Loader to imges due to slow load od image
 		* replacing the attribute of content or summary
 		*/
-		// var applyImageLoaderToDetailFeedImages = function(data) {
-		// 	$log.debug(data);
-		// 	var elem= document.createElement("div");
-		// 	elem.innerHTML = data.feed.content || data.feed.summary;
-		// 	var images = elem.getElementsByTagName("img");
-		// 	angular.forEach(images, function(val,key){
-		// 		val.setAttribute("image-lazy-src", val.src);
-		// 		val.setAttribute("image-lazy-loader", "android");
-		// 		val.setAttribute("image-lazy-distance-from-bottom-to-load",100);
-		// 		val.removeAttribute("src");
-		// 		val.removeAttribute("border");
-		// 	});
-		// 	if(data.feed.content){
-		// 		data.feed.content = elem.innerHTML;
-		// 	}else {
-		// 		data.feed.summary = elem.innerHTML;
-		// 	}
-		// };
+		var applyImageLoaderToDetailFeedImages = function(data) {
+			$log.debug(data);
+			var elem= document.createElement("div");
+			elem.innerHTML = data.feed.content || data.feed.summary;
+			var images = elem.getElementsByTagName("img");
+			angular.forEach(images, function(val,key){
+				val.setAttribute("image-lazy-src", val.src);
+				val.setAttribute("image-lazy-loader", "android");
+				val.setAttribute("image-lazy-distance-from-bottom-to-load",100);
+				val.removeAttribute("src");
+				val.removeAttribute("border");
+			});
+			if(data.feed.content){
+				data.feed.content = elem.innerHTML;
+			}else {
+				data.feed.summary = elem.innerHTML;
+			}
+		};
 
 	    /**
 	    * getting the current index of th feed showing in article page
---
+++ www/js/factories/requestInterceptor.factory.js
@@ -0,0 +1,113 @@
+(function() {
+    'use strict';
+    var requestIntercepter = function($log, $q, $injector, messagesService, utilService) { 
+
+        return{
+            request: function(config) {
+                config.headers = config.headers || {};
+                
+                //if( ionic.Platform.isWebView() && angular.isDefined(localStorage.tatafo_deviceInfo) ){
+                if( ionic.Platform.isWebView() && angular.isDefined(localStorage.tatafo_deviceInfo) ){
+                    //set headers so that token can be verified and user's last activity can be captured
+                    var deviceInfo = angular.fromJson(localStorage.tatafo_deviceInfo);
+                    config.headers["deviceId"] = deviceInfo.device_id;
+                    config.headers['deviceToken'] = deviceInfo.device_token;
+                    config.headers['pushToken'] = deviceInfo.push_token;
+                }else{
+                    config.headers['test'] = 'true';
+                }
+
+                config.timeout = 60000;
+
+                return config;
+            },
+
+            response: function(response){
+                return response;
+            },
+
+            requestError: function(rejection) {
+                return rejection;
+            },
+            
+            responseError: function(rejection) {
+
+                var ConnectivityMonitorFactory = $injector.get('ConnectivityMonitorFactory');
+                // $log.debug(rejection);
+                if(angular.isDefined(window.Connection)) {
+
+                    if(navigator.connection.type == Connection.NONE) {
+                        ConnectivityMonitorFactory.showErrorBanner(messagesService.general.INTERNET_NOT_WORKING);
+                    }else {
+                        switch(rejection.status){
+                            case 0:
+                              //timeout case, server unreachable or internet not working
+                            ConnectivityMonitorFactory.showErrorBanner(messagesService.general.INTERNET_NOT_WORKING);
+                            break;
+
+                            case 400:
+                            utilService.toastOrDialog(rejection.data.error.message);
+                            case 401:
+                            //unauthorized 
+                            break;
+
+                            case 403:
+                            //access forbidden 
+                            utilService.toastOrDialog(rejection.data.error.message);
+                            break;
+
+                            case 404:
+                            utilService.toastOrDialog(rejection.data.error.message);
+                            break;
+
+                            case 412:
+                            //multiple error case
+                            if(angular.isString(rejection.data.error.message)) {
+                                utilService.toastOrDialog(rejection.data.error.message);
+                            }else{                    
+                                var firstKey = "";
+                                for (var keys in rejection.data.error.message) {
+                                    firstKey = keys;
+                                    break;
+                                }
+                                if(angular.isArray(rejection.data.error.message[firstKey])) {
+                                    utilService.toastOrDialog(rejection.data.error.message[firstKey][0]);
+                                }else {
+                                    utilService.toastOrDialog(rejection.data.error.message[firstKey]);
+                                }
+                            }
+                            break;
+
+                            case 500:
+                            if(angular.isDefined(rejection.data.errors)) {
+                                utilService.toastOrDialog(rejection.data.errors.message);
+                            }else {
+                                utilService.toastOrDialog(rejection.data.message);
+                            }
+                            break;
+
+                            default:
+                           // utilService.toastOrDialog(messagesService.general.SOMETHING_WRONG);
+                            break;
+                        }//end of switch
+
+                    } //end else block
+                }else{
+                    $log.debug("window.Connection not defined.");
+                } //end if(window.connection) block
+
+                return $q.reject(rejection);
+            } 
+        };
+
+    }
+    
+    requestIntercepter.$inject = ['$log', '$q', '$injector' , 'messagesService' , 'utilService'];
+    angular
+        .module('tatafo')
+        .factory('requestIntercepter', requestIntercepter)
+        .config(function($httpProvider) {
+            //$httpProvider.defaults.headers['hello'] = 'work';
+            $httpProvider.interceptors.push('requestIntercepter');
+        });
+})();
