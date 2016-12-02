70e68bc053c694d1c4ae5dd49bba235826f7caa7
Manpreet <manpreet.singh@logicielsolutions.co.in>
2016-12-02 18:42:21 +0530

    configs changed

--- www/js/config.js
+++ www/js/config.js
@@ -6,11 +6,13 @@ angular
 	* Constant ONESIGNAL App Id
 	* Constant for Admob (Banner and Interstitial ads)
 	**/
-	.constant('TATAFO_API_URL','http://qa.tatafo.me/api/v1')
-	.constant('ONESIGNAL_APP_ID','8c566d45-c299-4535-bd2f-f19415635a55')
-	.constant('GOOGLE_PROJECT_NUMBER','1082216437947')
-	.constant('ANDROID_BANNER_ID','ca-app-pub-6869992474017983/9375997553')
-	.constant('ANDROID_INTERSTITIAL_ID','ca-app-pub-6869992474017983/1657046752')
+	// .constant('TATAFO_API_URL','http://qa.tatafo.me/api/v1')
+	// .constant('ONESIGNAL_APP_ID','8c566d45-c299-4535-bd2f-f19415635a55')
+	// .constant('GOOGLE_PROJECT_NUMBER','1082216437947')
+	// .constant('ANDROID_BANNER_ID','ca-app-pub-6869992474017983/9375997553')
+	// .constant('ANDROID_INTERSTITIAL_ID','ca-app-pub-6869992474017983/1657046752')
+	// .constant('DEBUG', true)
+
 
 
 	/**
@@ -18,9 +20,14 @@ angular
 	* Constant ONESIGNAL App Id
 	* Constant for Admob (Banner and Interstitial ads)
 	**/
-	// .constant('TATAFO_API_URL','http://qa.tatafo.me/api/v1')
-	// .constant('ONESIGNAL_APP_ID','8c566d45-c299-4535-bd2f-f19415635a55')
-	// .constant('GOOGLE_PROJECT_NUMBER','1082216437947')
-	// .constant('ANDROID_BANNER_ID','ca-app-pub-6869992474017983/9375997553')
-	// .constant('ANDROID_INTERSTITIAL_ID','ca-app-pub-6869992474017983/1657046752')
+	.constant('TATAFO_API_URL','http://tatafo.me/api/v1')
+	.constant('ONESIGNAL_APP_ID','a726a388-d2df-4a3f-8d02-e4a1a5cb6c2e')
+	.constant('GOOGLE_PROJECT_NUMBER','581044149896')
+	.constant('ANDROID_BANNER_ID','ca-app-pub-6869992474017983/9375997553')
+	.constant('ANDROID_INTERSTITIAL_ID','ca-app-pub-6869992474017983/1657046752')
+	.constant('DEBUG', true)
 	
+	.config(function($logProvider, DEBUG){
+		//enable disable log statements depends on environment DEBUG flag
+	  	$logProvider.debugEnabled(DEBUG);
+	});
